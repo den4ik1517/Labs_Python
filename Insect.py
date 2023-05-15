ea6b193e05f7147272f613c4a30fc6cd4658cf09
den4ik1517 <119353622+den4ik1517@users.noreply.github.com>
2023-05-15 14:12:53 +0300

    Update Insect.py

--- Insect.py
+++ Insect.py
@@ -1,5 +1,5 @@
 class Insect:
-    instance = None
+    __instance = None
 
     def __init__(self, name="", number_of_legs=6, has_wings=False, is_dangerous=False, is_sleeping=False):
         self.name = name
@@ -10,9 +10,9 @@ class Insect:
 
     @staticmethod
     def get_instance():
-        if not Insect.instance:
-            Insect.instance = Insect()
-        return Insect.instance
+        if not Insect.__instance:
+            Insect.__instance = Insect()
+        return Insect.__instance
 
     @staticmethod
     def is_poisonous():
@@ -28,10 +28,10 @@ class Insect:
         return f"Insect(name={self.name}, number_of_legs={self.number_of_legs}, has_wings={self.has_wings}, is_dangerous={self.is_dangerous}, is_sleeping={self.is_sleeping})"
 
 
-insects = [
-    Insect("Bee", 6, True, True, True),
-]
+if __name__ == '__main__':
+    insects = [
+        Insect("Bee", 6, True, True, True),
+    ]
 
-for insect in insects:
-    print(insect)
-    
+    for insect in insects:
+        print(insect)
